55d12720a46d90ad2a224ca33bf8f12e752a4d32
Jospaul Mahajan Prakash <jprakash@jcp.com>
2019-03-14 17:35:31 -0500

    updated climbing ladder

--- climbingladder.py
+++ climbingladder.py
@@ -9,27 +9,26 @@ import sys
 # Complete the climbingLeaderboard function below.
 def climbingLeaderboard(scores, alice):
     rank = []
-    currank = 1
-    prevrank = 0
+    currank = len(set(scores)) + 1
+    j = len(scores) - 1
+    limit = len(scores) - 1
     for i in range(len(alice)):
-        j = i
-        while j < len(scores):
-            if alice[i] >= scores[j]:
-                if j == 0:
-                    currank = 1
-                elif j > 0:
-                    if scores[j] < scores[j-1]:
-                        currank = currank - 1
-            elif alice[i] == scores[j]:
-                currank = prevrank
-            elif alice[i] < scores[j]:
-                if j > 0:
-                    if scores[j] < scores[j-1]:
-                        currank = currank + 1
-                else:
-                    currank = currank + 1
-            prevrank = currank
-            j = j+1
+        count = 0
+        while alice[i] >= scores[j]:
+            if (j >= 0) & (j < (len(scores) - 1)):
+                if (scores[j] <= scores[j-1]) & (scores[j] > scores[j+1]):
+                    count = count + 1
+            elif j >= 0:
+                if scores[j] < scores[j - 1]:
+                    count = count + 1
+            else:
+                break
+            j = j - 1
+
+        limit = limit - count
+        j = limit
+        if count > 0:
+            currank = currank - count
         rank.append(currank)
     return rank
 
